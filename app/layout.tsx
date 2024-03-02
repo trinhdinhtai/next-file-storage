04fa0b870c467601901cc6deee1bdea5da8b10d4
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-02 22:29:21 +0700

    feat: add new dependencies and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
+import { Toaster } from "@/components/ui/sonner"
 import ConvexClientProvider from "@/components/providers/ConvexClientProvider"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ConvexClientProvider>
           {children}
+          <Toaster />
           <TailwindIndicator />
         </ConvexClientProvider>
       </body>
