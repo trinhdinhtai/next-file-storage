9a31ffbe622c8a4e63922b359bfb9d60cec9b9b3
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-02 22:20:28 +0700

    feat: add Clerk integration and Convex auth config

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// https://docs.convex.dev/auth/clerk
+export default {
+  providers: [
+    {
+      domain: "https://pumped-sole-24.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+}
