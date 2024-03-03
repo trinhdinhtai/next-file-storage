0e737abd3aad8912fc81a149fb8f1af1c9aae40a
taitddev <142890841+trinhdinhtai@users.noreply.github.com>
2024-03-03 22:07:07 +0700

    feat: add files module to fullApi

---
+++ convex/files.ts
@@ -0,0 +1,14 @@
+import { v } from "convex/values"
+
+import { mutation } from "./_generated/server"
+
+export const createFile = mutation({
+  args: {
+    name: v.string(),
+  },
+  async handler(ctx, args) {
+    await ctx.db.insert("file", {
+      name: args.name,
+    })
+  },
+})
